5f73dd98a736fe3d1ea70bbbddcb97acc4824d7e
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-08-19 22:04:46 -0500

    Add Extension Method

---
+++ EventAggregatorRx.WPF46/EventAggregatorExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventAggregatorRx
+{
+    public static class EventAggregatorExtension
+    {
+        public static void Publish<TEvent>(this IEventAggregator eventAggregator) where TEvent : IEvent, new() 
+            => eventAggregator.Publish(new TEvent());
+
+        public static void Publish<TEvent>(this IEventAggregator eventAggregator, Action<TEvent> action) where TEvent : IEvent, new()
+        {
+            if (action == null) throw new ArgumentNullException($"Action cannot be null");
+
+            var @event = new TEvent();
+            action?.Invoke(@event);
+            eventAggregator.Publish(@event);
+        }
+    }
+}
